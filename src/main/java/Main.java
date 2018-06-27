a6ccd7e7cde980454a006e56d96493dfb7ef28e7
BrandonDudek <19555354+BrandonDudek@users.noreply.github.com>
2018-06-27 13:39:55 -0400

    Update Main.java
    Added Driver Cleanup steps.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,25 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.support.ui.Quotes;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ////////// Create Driver and Launch Browser //////////
-        final File IE_DRIVER = new File(Thread.currentThread().getContextClassLoader().getResource("IEDriverServer_x32_3.13.0.exe").getFile());
+        final String DRIVER_NAME = "IEDriverServer_x32_3.12.0.exe";
+
+        try {
+            Runtime.getRuntime().exec("taskkill /T /F /IM " + DRIVER_NAME);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final File IE_DRIVER = new File(Thread.currentThread().getContextClassLoader().getResource(DRIVER_NAME).getFile());
         System.setProperty("webdriver.ie.driver", IE_DRIVER.getAbsolutePath());
 
         InternetExplorerOptions options = new InternetExplorerOptions();
@@ -23,18 +32,30 @@ public class Main {
         DRIVER.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         DRIVER.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 
-        DRIVER.get("https://www.google.com/");
+        for(int i = 0; i < 15; i++) {
 
-        for(int i = 0; i < 100; i++) {
-
+            DRIVER.get("https://www.google.com/");
             WebElement searchInput = DRIVER.findElement(By.id("lst-ib"));
             searchInput.click(); // ERROR HERE! (intermittent)
-            searchInput.sendKeys("WALLA " + Keys.ESCAPE);
+            searchInput.sendKeys("Walla Walla Walla" + Keys.ESCAPE);
+
+            try {
+                Thread.sleep(500);
+            }
+            catch(InterruptedException e) {
+                // Ignore.
+            }
 
             WebElement searchButton = DRIVER.findElement(By.cssSelector("#tsf input[value='Google Search']"));
             searchButton.click(); // ERROR HERE! (intermittent)
         }
 
         DRIVER.quit();
+
+        try {
+            Runtime.getRuntime().exec("taskkill /T /F /IM " + DRIVER_NAME);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
