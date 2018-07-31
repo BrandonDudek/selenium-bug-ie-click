62c6adb646992cb180bca41a97c4b5af3358e8e4
BrandonDudek <19555354+BrandonDudek@users.noreply.github.com>
2018-07-31 07:29:35 -0400

    Fixed in IEDriverServer 3.13.0.5!

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        final String DRIVER_NAME = "IEDriverServer_x32_3.13.0.exe";
+        final String DRIVER_NAME = "IEDriverServer_x32_3.13.0.5.exe";
 
         try {
             Runtime.getRuntime().exec("taskkill /T /F /IM " + DRIVER_NAME);
